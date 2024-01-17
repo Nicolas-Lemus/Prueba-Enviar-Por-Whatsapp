a4230d1ca01889d617b8577d6797d4e17f69d083
Nicolas Lemus <nicolaswaldemarlemuscurbelo@gmail.com>
2024-01-16 23:19:18 -0300

    realizamos implementacion de modal informativo en pagina de inicio

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -5,19 +5,17 @@ import { CartContext } from "../../context/CartContext";
 import { Link } from "react-router-dom";
 import "./CartWidget.css"
 
-
   const CartWidget = () => {
 
   const [quantity,setQuantity] = useState(0);
   const {count} = useContext(CartContext);
 
   useEffect(() => {
-    
     setQuantity(count.tecnologia.reduce((total, product) => total + product.quantity, 0));
   }, [count]);
   return (
     <div>
-      <Link to="/cart">
+      <Link  to="/cart">
         <FontAwesomeIcon className="icon" icon={faCartShopping}/>
         <span className="cartQuantity">{quantity}</span>
       </Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+/* import React from 'react'
 import Headers from '../components/Headers/Headers'
 import ControlledCarousel from '../components/Carousels/ControlledCarousel';
 import Welcome from '../components/Welcome/Welcome';
@@ -14,4 +14,37 @@ const Home = () => {
     )
 };
 
-export default Home
+export default Home;
+
+ */
+
+import React, { useState } from 'react';
+import Headers from '../components/Headers/Headers';
+import ControlledCarousel from '../components/Carousels/ControlledCarousel';
+import Welcome from '../components/Welcome/Welcome';
+
+const Home = () => {
+  const [showAlert, setShowAlert] = useState(true);
+
+  const handleAccept = () => {
+    setShowAlert(false);
+  };
+
+  return (
+    <div>
+        {showAlert && (
+            <div className="alert">
+                <p>Esta página no es una página de compras</p>
+                <p>Puede realizar sus consultas mediante WhatsApp</p>
+                <button onClick={handleAccept}>Aceptar</button>
+            </div>
+        )}
+
+        <Headers HeadersH1="Tienda Fashion" />      
+        <ControlledCarousel />
+        <Welcome />
+    </div>
+  );
+};
+
+export default Home;
